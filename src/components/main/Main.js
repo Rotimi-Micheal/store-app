8d565fc1a0849c302dca4c29021098fc880dbc7b
Rotimi-Micheal <mikelater8@gmail.com>
2022-12-26 02:21:52 +0100

    new feature

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,21 @@
-import "./Main.css";
+import classes from "./Main.module.css";
 
 const Main = () => {
   return (
     <section>
-      <h1 className="title">Welcome Page</h1>
+      <div className={classes["welcome-page"]}>
+        <h1>Welcome to our website!</h1>
+        <p>
+          We are glad you are here. Our website includes a blog where we share
+          updates and interesting articles, as well as a store where you can
+          purchase products related to our business.
+        </p>
+        <p>
+          Take a look around and let us know if you have any questions or need
+          help finding something.
+        </p>
+        <p>Thank you for visiting.</p>
+      </div>
     </section>
   );
 };
